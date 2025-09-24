dffd50f636caaa4db5210ef9f09c114061b6619d
tomekstrand78@gmail.com <tomekstrand78@gmail.com@XNENON02>
2025-09-24 11:50:07 +0200

    just added a to do list for me self

---
+++ OOPAssignment/Toms_Lager/OrderList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toms_Lager
+{
+    internal class OrderList
+    {
+
+        //to do add a sytem that can find and read an order from a csv file and subtrack or add if canceld order back in to the lager 
+
+
+
+
+
+
+
+
+    }
+}
