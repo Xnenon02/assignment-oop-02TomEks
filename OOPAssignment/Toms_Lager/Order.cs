0d28179e5180f47e6e837b1215af51acb97d5422
tomekstrand78@gmail.com <tomekstrand78@gmail.com@XNENON02>
2025-09-24 14:45:35 +0200

    80% done i swaer

--- OOPAssignment/Toms_Lager/Order.cs
+++ OOPAssignment/Toms_Lager/Order.cs
@@ -3,7 +3,7 @@
 namespace Toms_Lager
 {
     public class Order
-    {
+    { // för dig som undar så är detta bara en typ av liten data holder
         public string KundNamn { get; set; }
         public int ProduktId { get; set; }
         public int Antal { get; set; }
